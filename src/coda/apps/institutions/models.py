5b2dd3c4b3f646e64024f87351fada410ace4a04
Sven Marcus <s.marcus@outlook.de>
2024-01-29 11:24:09 +0000

    feat(institutions): adds initial institutons app with model

---
+++ src/coda/apps/institutions/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
+    )
