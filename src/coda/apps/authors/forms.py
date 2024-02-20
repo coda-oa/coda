b50924d98830fc17618213de82c4b8e60e1e8641
Sven Marcus <s.marcus@outlook.de>
2024-02-20 15:52:13 +0000

    feat(fundingrequests): more work on fundingrequest views

--- src/coda/apps/authors/forms.py
+++ src/coda/apps/authors/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
+from coda.apps.institutions.models import Institution
+
 
 class PersonForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     orcid = forms.CharField()
+
+
+class InstitutionForm(forms.Form):
+    name = forms.ChoiceField(choices=[(i.pk, i.name) for i in Institution.objects.all()])
--- src/coda/apps/fundingrequests/views.py
+++ src/coda/apps/fundingrequests/views.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from django.views.generic import DetailView, ListView, TemplateView
 
-from coda.apps.authors.forms import PersonForm
+from coda.apps.authors.forms import InstitutionForm, PersonForm
 from coda.apps.fundingrequests.forms import FundingRequestForm
 from coda.apps.fundingrequests.models import FundingRequest
 from coda.apps.publications.forms import LinkForm, PublicationForm
@@ -18,6 +18,7 @@ class FundingRequestListView(ListView[FundingRequest]):
     model = FundingRequest
     template_name = "fundingrequests/fundingrequest_list.html"
     context_object_name = "funding_requests"
+    paginate_by = 10
 
 
 class FundingRequestCreateView(TemplateView):
@@ -25,7 +26,10 @@ class FundingRequestCreateView(TemplateView):
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         ctx = super().get_context_data(**kwargs)
+        ctx["submitter_form"] = PersonForm()
+        ctx["submitter_affiliation_form"] = InstitutionForm()
         ctx["author_form"] = PersonForm()
+        ctx["author_affiliation_form"] = InstitutionForm()
         ctx["publication_form"] = PublicationForm()
         ctx["link_form"] = LinkForm()
         ctx["fundingrequest_form"] = FundingRequestForm()
