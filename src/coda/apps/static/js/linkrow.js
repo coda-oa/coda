fde3c0616c8c8b5af2e0a09481ab1654f5e468a2
Sven Marcus <s.marcus@outlook.de>
2024-02-28 15:19:08 +0000

    refactor(js): drop alpine js in favor of regular js for now

---
+++ src/coda/apps/static/js/linkrow.js
@@ -0,0 +1,30 @@
+let linkTable = null;
+let slots = null;
+
+window.addEventListener('DOMContentLoaded', () => {
+  linkTable = document.getElementById('linktable');
+  slots = linkTable.querySelector('template');
+});
+
+function addRow() {
+  const templateClone = slots.content.cloneNode(true);
+  const row = templateClone.querySelector('tr');
+  addRemoveRowButton(row);
+  linkTable.append(row);
+}
+
+function addRemoveRowButton(row) {
+  const td = document.createElement('td');
+  td.appendChild(removeRowButton(row));
+  row.appendChild(td);
+}
+
+function removeRowButton(row) {
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.textContent = 'Remove';
+  button.addEventListener('click', () => {
+    linkTable.removeChild(row);
+  });
+  return button;
+}
