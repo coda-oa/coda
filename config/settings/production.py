39a4c72493f100b17ca8547df0529d66e1eee558
Sven Marcus <s.marcus@outlook.de>
2024-04-11 10:15:58 +0200

    build(settings): allow setting trusted csrf origins

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,7 +9,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", default=str(uuid.uuid4()))
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[])
 
-# SUBDIRECTORY SETTINGS
+
 # ------------------------------------------------------------------------------
 USE_X_FORWARDED_HOST = env.bool("DJANGO_USE_X_FORWARDED_HOST", default=False)
 FORCE_SCRIPT_NAME = env.str("DJANGO_FORCE_SCRIPT_NAME", default=None)
@@ -36,6 +36,8 @@ DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # no
 
 # SECURITY
 # ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-trusted-origins
+CSRF_TRUSTED_ORIGINS = env.list("DJANGO_CSRF_TRUSTED_ORIGINS", default=[])
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
