f9318e08362c9075515d238d46c41dd813604c04
Sven Marcus <s.marcus@outlook.de>
2024-04-16 18:07:00 +0000

    feat(authorlist): begin implementing author list to split author strings taken from papers

---
+++ src/coda/authorlist.py
@@ -0,0 +1,38 @@
+import re
+
+
+class AuthorList(list[str]):
+    @classmethod
+    def from_str(cls, authors: str) -> "AuthorList":
+        authors = cls._insert_missing_space(authors)
+        authors = cls._replace_broken_umlaute(authors)
+        author_lines = authors.strip().splitlines()
+        reverse = "," in authors and ";" in authors
+        return cls(
+            author
+            for line in author_lines
+            for author in cls._parse_line(line, reverse_names=reverse)
+            if author
+        )
+
+    @staticmethod
+    def _parse_line(line: str, /, reverse_names: bool) -> list[str]:
+        if reverse_names:
+            split_authors_by_semicolon = map(str.strip, line.split(";"))
+            split_names_by_comma = [x.split(",") for x in split_authors_by_semicolon]
+            reversed_names = [reversed(x) for x in split_names_by_comma]
+            split_authors = [" ".join(x).strip() for x in reversed_names]
+        else:
+            sep = "," if "," in line else ";"
+            line = line.replace(", and ", sep).replace(" and ", sep)
+            split_authors = [*map(str.strip, line.split(sep))]
+
+        return split_authors
+
+    @staticmethod
+    def _insert_missing_space(author: str) -> str:
+        return re.sub(r"([a-z])([A-Z])", r"\1 \2", author)
+
+    @staticmethod
+    def _replace_broken_umlaute(author: str) -> str:
+        return author.replace(" ̈u", "ü").replace(" ̈o", "ö").replace(" ̈a", "ä")
---
+++ tests/test_authorlist.py
@@ -0,0 +1,106 @@
+from typing import NamedTuple
+
+import pytest
+
+from coda.authorlist import AuthorList
+
+csv_name_list_no_spaces = "John Doe,Jane Doe,Alice Doe"
+csv_name_list_with_spaces = "John Doe, Jane Doe, Alice Doe"
+csv_name_list_last_author_sep_with_comma_and = "John Doe, Jane Doe, and Alice Doe"
+csv_name_list_last_author_sep_with_only_and = "John Doe, Jane Doe and Alice Doe"
+semicolon_sep_name_list = "John Doe; Jane Doe; Alice Doe"
+semicolon_sep_reversed_name_list = "Doe, John; Doe, Jane; Doe, Alice"
+newline_sep_name_list = """
+    John Doe
+    Jane Doe
+    Alice Doe
+"""
+
+csv_name_list_with_line_breaks = """
+    John Doe,
+    Jane Doe,
+    Alice Doe
+"""
+csv_name_list_missing_space_in_name = "John Doe, Jane Doe, AliceDoe"
+
+
+class DataPair(NamedTuple):
+    actual: str
+    expected: list[str]
+
+
+real_world_examples = [
+    DataPair(
+        """Alexander H. Miller, Will Feng, Adam Fisch, Jiasen Lu,
+Dhruv Batra, Antoine Bordes, Devi Parikh and Jason Weston""",
+        [
+            "Alexander H. Miller",
+            "Will Feng",
+            "Adam Fisch",
+            "Jiasen Lu",
+            "Dhruv Batra",
+            "Antoine Bordes",
+            "Devi Parikh",
+            "Jason Weston",
+        ],
+    ),
+    DataPair(
+        "Hafedh Mili, Fatma Mili, and Ali Mili",
+        ["Hafedh Mili", "Fatma Mili", "Ali Mili"],
+    ),
+    DataPair(
+        """Jorge J. Moré
+Stephen J. Wright""",
+        ["Jorge J. Moré", "Stephen J. Wright"],
+    ),
+    DataPair(
+        """Christopher Blech, Nils Dreyer, Bj ̈orn Friebel, Christoph R. Jacob, Mostafa Shamil Jassim, Leander Jehl, R ̈udigerKapitza, Manfred Krafczyk, Thomas K ̈urner, Sabine C. Langer, Jan Linxweiler∗, Mohammad Mahhouk, SvenMarcus, Ines Messadi, S ̈oren Peters, Jan-Marc Pilawa, Harikrishnan K. Sreekumar, Robert Str ̈otgen, Katrin Stump,Arne Vogel, Mario Wolter""",
+        [
+            "Christopher Blech",
+            "Nils Dreyer",
+            "Björn Friebel",
+            "Christoph R. Jacob",
+            "Mostafa Shamil Jassim",
+            "Leander Jehl",
+            "Rüdiger Kapitza",
+            "Manfred Krafczyk",
+            "Thomas Kürner",
+            "Sabine C. Langer",
+            "Jan Linxweiler∗",
+            "Mohammad Mahhouk",
+            "Sven Marcus",
+            "Ines Messadi",
+            "Sören Peters",
+            "Jan-Marc Pilawa",
+            "Harikrishnan K. Sreekumar",
+            "Robert Strötgen",
+            "Katrin Stump",
+            "Arne Vogel",
+            "Mario Wolter",
+        ],
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "authors",
+    [
+        csv_name_list_no_spaces,
+        csv_name_list_with_spaces,
+        csv_name_list_last_author_sep_with_comma_and,
+        csv_name_list_last_author_sep_with_only_and,
+        semicolon_sep_name_list,
+        semicolon_sep_reversed_name_list,
+        newline_sep_name_list,
+        csv_name_list_missing_space_in_name,
+    ],
+)
+def test__author_list__from_str__creates_author_list_param(authors: str) -> None:
+    sut = AuthorList.from_str(authors)
+    assert sut == ["John Doe", "Jane Doe", "Alice Doe"]
+
+
+@pytest.mark.parametrize("data_pair", real_world_examples)
+def test__author_list__from_str__creates_author_list_param_real_world(data_pair: DataPair) -> None:
+    sut = AuthorList.from_str(data_pair.actual)
+    assert sut == data_pair.expected
