020f65fddfa7a6b40b57278b8059dfd42adec71b
Sven Marcus <s.marcus@outlook.de>
2024-06-20 16:39:07 +0000

    feat(add_test_journals): parses contracts from journals.csv

--- src/coda/apps/management/management/commands/add_test_journals.py
+++ src/coda/apps/management/management/commands/add_test_journals.py
@@ -1,9 +1,11 @@
+import datetime
 from typing import Any
 
 import polars as pl
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
+from coda.apps.contracts.models import Contract
 from coda.apps.journals.models import Journal
 from coda.apps.publishers.models import Publisher
 
@@ -17,6 +19,7 @@ class Command(BaseCommand):
         df = self.download_test_data()
         self.add_publishers(df)
         self.add_journals(df)
+        self.add_contracts(df)
 
     def delete_old_data(self) -> None:
         self.stdout.write("Delete all journals")
@@ -26,6 +29,10 @@ class Command(BaseCommand):
         self.stdout.write("Delete all publishers")
         publishers = Publisher.objects.all()
         publishers.delete()
+
+        self.stdout.write("Delete all contracts")
+        contracts = Contract.objects.all()
+        contracts.delete()
 
     def download_test_data(self) -> pl.DataFrame:
         self.stdout.write("Downloading journals from GitHub")
@@ -55,3 +62,21 @@ class Command(BaseCommand):
                 for row in df.rows(named=True)
             ]
         )
+
+    def add_contracts(self, df: pl.DataFrame) -> None:
+        self.stdout.write("Adding Contracts")
+        journals_with_contract = df.filter(pl.col("contract").is_not_null()).sort("e_issn")
+        e_issns = set(journals_with_contract["e_issn"].unique().to_list())
+        journals = Journal.objects.filter(eissn__in=e_issns).order_by("eissn")
+        for row, journal in zip(journals_with_contract.rows(named=True), journals):
+            assert journal.eissn == row["e_issn"]
+            contract_name = row["contract"]
+            contract, created = Contract.objects.get_or_create(name=contract_name)
+            if created:
+                contract.start_date = datetime.date(2024, 1, 1)
+                contract.end_date = datetime.date(2028, 12, 31)
+
+            contract.publishers.add(journal.publisher)
+            journal.contracts.add(contract)
+            journal.save()
+            contract.save()
