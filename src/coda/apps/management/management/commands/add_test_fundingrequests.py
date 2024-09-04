a5b975d06cd046d2ded0ac5e05456b352bca081f
Sven Marcus <s.marcus@outlook.de>
2024-09-04 12:07:17 +0000

    feat(publication): add publication_type and subject_area parameters to constructor
    uses new params in add_test_fundingrequests

--- src/coda/apps/management/management/commands/add_test_fundingrequests.py
+++ src/coda/apps/management/management/commands/add_test_fundingrequests.py
@@ -10,6 +10,7 @@ from faker.providers import lorem
 from coda.apps.fundingrequests.models import FundingOrganization
 from coda.apps.fundingrequests.services import fundingrequest_create, fundingrequest_perform_review
 from coda.apps.journals.models import Journal
+from coda.apps.preferences.models import GlobalPreferences
 from coda.apps.publications.models import LinkType
 from coda.apps.publishers.models import Publisher
 from coda.author import Author, AuthorList, Role
@@ -23,7 +24,16 @@ from coda.fundingrequest import (
     Review,
 )
 from coda.money import Currency, Money
-from coda.publication import JournalId, License, OpenAccessType, Publication, Published
+from coda.publication import (
+    ConceptId,
+    JournalId,
+    License,
+    OpenAccessType,
+    Publication,
+    Published,
+    VocabularyConcept,
+    VocabularyId,
+)
 from coda.string import NonEmptyStr
 
 faker = Faker()
@@ -51,6 +61,11 @@ class Command(BaseCommand):
     ) -> None:
         journal = self.journal()
         _ = LinkType.objects.get_or_create(name="DOI")
+        subject_area_vocabulary = GlobalPreferences.get_subject_classification_vocabulary()
+        publication_types_vocabulary = GlobalPreferences.get_publication_type_vocabulary()
+
+        random_subject = random.choice(subject_area_vocabulary.concepts.all())
+        random_publication_type = random.choice(publication_types_vocabulary.concepts.all())
 
         request = FundingRequest.new(
             Publication.new(
@@ -62,6 +77,14 @@ class Command(BaseCommand):
                 publication_state=Published(
                     date.fromisoformat(faker.date()),
                     date.fromisoformat(faker.date()),
+                ),
+                subject_area=VocabularyConcept(
+                    id=ConceptId(random_subject.concept_id),
+                    vocabulary=VocabularyId(random_subject.vocabulary.pk),
+                ),
+                publication_type=VocabularyConcept(
+                    id=ConceptId(random_publication_type.concept_id),
+                    vocabulary=VocabularyId(random_publication_type.vocabulary.pk),
                 ),
                 links={Doi("10.1234/5678")},
             ),
