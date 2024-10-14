fc1ac509c8beb435bf91a1cbfc7775fc3f7520fd
Sven Marcus <s.marcus@outlook.de>
2024-10-14 05:33:11 +0000

    feat(HtmxDynamicFormset): more work on adding/removing forms dynamically

--- src/coda/apps/htmx/forms.py
+++ src/coda/apps/htmx/forms.py
@@ -1,7 +1,8 @@
+import logging
 from typing import Any, cast
 
 from django import forms
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, QueryDict
 from django.template import Context, Engine, Template, engines
 from django.template.response import TemplateResponse
 from django.utils.datastructures import MultiValueDict
@@ -11,12 +12,13 @@ from django.views import View
 def _template_string(url_name: str) -> str:
     return f"""
     <div class="htmx-formset-wrapper">
+        <p>{{{{ total_forms }}}} forms</p>
         <input type="hidden" name="total_forms" value="{{{{ total_forms }}}}">
         {{% for form in formset %}}
             {{{{ form.as_p }}}}
-            <button type="button" hx-swap="outerHTML" hx-target="closest div.htmx-formset-wrapper">Remove</button>
+            <button type="button" name="form_action_delete" value="{{{{ forloop.counter }}}}" hx-post="{{% url '{url_name}' %}}" hx-swap="outerHTML" hx-target="closest div.htmx-formset-wrapper">Remove</button>
         {{% endfor %}}
-        <button type="button" hx-post="{{% url '{url_name}' %}}" hx-swap="outerHTML" hx-target="closest div.htmx-formset-wrapper">Add</button>
+        <button type="button" name="form_action_add" value="add_form" hx-post="{{% url '{url_name}' %}}" hx-swap="outerHTML" hx-target="closest div.htmx-formset-wrapper">Add</button>
     </div>
     """
 
@@ -30,29 +32,47 @@ class ManagementView(View):
     min_forms: int = 1
 
     def get(self, request: HttpRequest) -> HttpResponse:
-        return TemplateResponse(
-            request, self.get_template(), self.get_context(request, self.min_forms)
-        )
+        return self._get_response(request, self.min_forms)
 
     def post(self, request: HttpRequest) -> HttpResponse:
+        if request.POST.get("form_action_add") is not None:
+            return self._get_response(request, self.total_forms() + 1)
+        elif (form_index := request.POST.get("form_action_delete")) is not None:
+            formset = self._get_forms(request, int(form_index))
+            logging.error(f"Prefixes {[f.prefix for f in formset]}")
+            for i, f in enumerate(formset, start=1):
+                logging.error(f"Setting prefix of form '{f.prefix}' to 'form-{i}'")
+                f.prefix = f"form-{i}"
+            context = {"total_forms": len(formset), "formset": formset}
+            return TemplateResponse(request, self._get_template(), context)
+
+        else:
+            return self._get_response(request, self.total_forms())
+
+    def _get_forms(self, request: HttpRequest, exclude_index: int) -> list[forms.Form]:
+        return [
+            self.form_class(request.POST, prefix=f"form-{form_index}")
+            for form_index in range(1, self.total_forms() + 1)
+            if form_index != exclude_index
+        ]
+
+    def _get_response(self, request: HttpRequest, total_forms: int) -> HttpResponse:
         return TemplateResponse(
-            request, self.get_template(), self.get_context(request, self.total_forms + 1)
+            request, self._get_template(), self.get_context(request, total_forms)
         )
 
-    def delete(self, request: HttpRequest) -> HttpResponse:
-        return TemplateResponse(
-            request, self.get_template(), self.get_context(request, self.total_forms - 1)
-        )
-
-    @property
-    def total_forms(self) -> int:
-        return int(self.request.POST.get("total_forms", self.min_forms))
+    def total_forms(self, query_dict: QueryDict | None = None) -> int:
+        query_dict = query_dict or self.request.POST
+        return int(query_dict.get("total_forms", self.min_forms) or self.min_forms)
 
     def get_context(self, request: HttpRequest, total_forms: int) -> dict[str, Any]:
-        formset = [self.form_class(request.POST) for _ in range(int(total_forms))]
+        formset = [
+            self.form_class(request.POST, prefix=f"form-{form_index + 1}")
+            for form_index in range(total_forms)
+        ]
         return {"total_forms": total_forms, "formset": formset}
 
-    def get_template(self) -> Template:
+    def _get_template(self) -> Template:
         # TODO: This cast is incorrect as BACKEND.from_string returns a _EngineTemplate, not a regular Template
         return cast(Template, BACKEND.from_string(_template_string(self.name)))
 
@@ -68,6 +88,9 @@ class HtmxDynamicFormset:
     ) -> None:
         self.data = data
         self.form_class = form_class or self.form_class
+        if not self.name:
+            raise ValueError("name is required")
+
         if not issubclass(self.form_class, forms.Form):
             raise TypeError("form_class must be a subclass of django.forms.Form")
 
@@ -83,7 +106,7 @@ class HtmxDynamicFormset:
         return (
             Engine.get_default()
             .from_string(_template_string(self.name))
-            .render(Context({"formset": [self.form_class()]}))
+            .render(Context({"formset": [self.form_class(prefix="form-1")], "total_forms": 1}))
         )
 
 
