b5763163ba0cddee1a507d3337b62020a1397784
Sven Marcus <s.marcus@outlook.de>
2024-12-03 16:34:36 +0000

    fix(fixtures): parse missing level of dfg subject classification. use lookup table for entity ids to keep them consistent when re-generating vocabulary fixtures
    BREAKING CHANGE: will break subject area/publication types linked to publications

--- scripts/vocabularies/dfg_subject_classification.py
+++ scripts/vocabularies/dfg_subject_classification.py
@@ -14,18 +14,22 @@ DFG_SUBJECT_CLASSIFICATION_PATH = (
 
 def process_excel() -> pl.DataFrame:
     df = pl.read_excel(DFG_SUBJECT_CLASSIFICATION_PATH, read_options={"header_row": 2})
-    return (
+    df = (
         df.with_columns(
             pl.col("Subject area").alias("Subject area ID"),
             pl.col("__UNNAMED__1").alias("Subject area name"),
+            pl.col("Review Board").str.replace("\n", " "),
             pl.col("__UNNAMED__3").alias("Subgroup").str.replace("\n", " "),
             pl.col("__UNNAMED__4").alias("Group").str.replace("\n", " "),
         )
         .with_columns(
             pl.col("Subgroup").fill_null(strategy="forward"),
             pl.col("Group").fill_null(strategy="forward"),
+            pl.col("Review Board").fill_null(strategy="forward"),
         )
         .with_columns(
+            pl.col("Review Board").str.slice(0, 4).str.strip_chars().alias("Review Board ID"),
+            pl.col("Review Board").str.slice(4).str.strip_chars().alias("Review Board name"),
             pl.col("Subgroup").str.slice(0, 2).str.strip_chars().alias("Subgroup ID"),
             pl.col("Subgroup").str.slice(3).str.strip_chars().alias("Subgroup name"),
             pl.col("Group").str.slice(0, 1).str.strip_chars().alias("Group ID"),
@@ -34,29 +38,31 @@ def process_excel() -> pl.DataFrame:
         .filter(pl.col("Subject area ID") != "Subject area")
         .drop(
             [
+                "Review Board",
                 "Subject area",
                 "Subgroup",
                 "Group",
-                "_duplicated_0",
-                "_duplicated_1",
-                "_duplicated_2",
-                "_duplicated_3",
+                "__UNNAMED__1",
+                "__UNNAMED__3",
+                "__UNNAMED__4",
             ],
             strict=False,
         )
     )
+    return df
 
 
 def parse_vocabulary() -> Vocabulary:
-    df = process_excel().sort("Group ID", "Subgroup ID", "Subject area ID")
+    df = process_excel().sort("Group ID", "Subgroup ID", "Review Board ID", "Subject area ID")
     groups = df.select("Group ID", "Group name").unique(keep="first", maintain_order=True)
     group_concepts = [group(df, row) for row in groups.iter_rows(named=True)]
 
-    return Vocabulary(
+    v = Vocabulary(
         name="DFG Subject Classification",
         version="2024-2028",
         concepts=group_concepts,
     )
+    return v
 
 
 def group(df: pl.DataFrame, group: dict[str, Any]) -> Concept:
@@ -82,7 +88,7 @@ def subgroups_for_group(df: pl.DataFrame, group_id: str) -> list[Concept]:
             id=row["Subgroup ID"],
             name=row["Subgroup name"],
             description="",
-            subconcepts=subjects_for_subgroup(df, row["Subgroup ID"]),
+            subconcepts=review_boards_for_subgroup(df, row["Subgroup ID"]),
         )
         for row in matching_subgroups.iter_rows(named=True)
     ]
@@ -90,8 +96,27 @@ def subgroups_for_group(df: pl.DataFrame, group_id: str) -> list[Concept]:
     return subgroup_concepts
 
 
-def subjects_for_subgroup(df: pl.DataFrame, subgroup_id: str) -> list[Concept]:
-    matching_subjects = df.filter(pl.col("Subgroup ID") == subgroup_id).select(
+def review_boards_for_subgroup(df: pl.DataFrame, subgroup_id: str) -> list[Concept]:
+    matching_review_boards = (
+        df.filter(pl.col("Subgroup ID") == subgroup_id)
+        .select("Review Board ID", "Review Board name")
+        .unique(keep="first", maintain_order=True)
+    )
+    review_board_concepts = [
+        Concept(
+            id=row["Review Board ID"],
+            name=row["Review Board name"],
+            description="",
+            subconcepts=subject_for_review_board(df, row["Review Board ID"]),
+        )
+        for row in matching_review_boards.iter_rows(named=True)
+    ]
+
+    return review_board_concepts
+
+
+def subject_for_review_board(df: pl.DataFrame, review_board_id: str) -> list[Concept]:
+    matching_subjects = df.filter(pl.col("Review Board ID") == review_board_id).select(
         "Subject area ID", "Subject area name"
     )
     subject_concepts = [
