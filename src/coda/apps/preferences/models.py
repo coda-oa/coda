a720bcfd0c9a7614a71f7e8ebc8ba93443929673
Sven Marcus <s.marcus@outlook.de>
2024-12-12 09:17:43 +0000

    feat(GlobalPreferences): introduce setting for monograph vocabulary
    not used in PublicationForm yet

--- src/coda/apps/preferences/models.py
+++ src/coda/apps/preferences/models.py
@@ -12,31 +12,37 @@ def empty_vocabulary() -> VocabularyModel:
     return VocabularyModel.empty()
 
 
-def default_subject_classification_vocabulary() -> VocabularyModel:
+def default_subject_classification_vocabulary() -> int:
     v = VocabularyModel.objects.filter(name="DFG Subject Classification").first()
     if not v:
-        return VocabularyModel.empty()
+        return VocabularyModel.empty().id
 
-    return v
+    return v.id
 
 
-def default_publication_type_vocabulary() -> VocabularyModel:
+def default_publication_type_vocabulary() -> int:
     v = VocabularyModel.objects.filter(name="COAR Resource Types").first()
     if not v:
-        return VocabularyModel.empty()
+        return VocabularyModel.empty().id
 
-    return v
+    return v.id
 
 
 class GlobalPreferences(models.Model):
     home_currency = models.CharField(max_length=255, default=Currency.EUR.code)
-    default_subject_classification_vocabulary = models.ForeignKey(
+    subject_classification_vocabulary = models.ForeignKey(
         VocabularyModel,
         on_delete=models.SET_DEFAULT,
         default=default_subject_classification_vocabulary,
         related_name="+",
     )
-    default_publication_type_vocabulary = models.ForeignKey(
+    article_publication_type_vocabulary = models.ForeignKey(
+        VocabularyModel,
+        on_delete=models.SET_DEFAULT,
+        default=default_publication_type_vocabulary,
+        related_name="+",
+    )
+    monograph_publication_type_vocabulary = models.ForeignKey(
         VocabularyModel,
         on_delete=models.SET_DEFAULT,
         default=default_publication_type_vocabulary,
@@ -46,14 +52,12 @@ class GlobalPreferences(models.Model):
     @staticmethod
     def get_subject_classification_vocabulary() -> VocabularyProtocol:
         prefs, _ = GlobalPreferences.objects.get_or_create()
-        return vocabulary_repository.as_domain_object(
-            prefs.default_subject_classification_vocabulary
-        )
+        return vocabulary_repository.as_domain_object(prefs.subject_classification_vocabulary)
 
     @staticmethod
     def get_publication_type_vocabulary() -> VocabularyProtocol:
         prefs, _ = GlobalPreferences.objects.get_or_create()
-        return vocabulary_repository.as_domain_object(prefs.default_publication_type_vocabulary)
+        return vocabulary_repository.as_domain_object(prefs.article_publication_type_vocabulary)
 
     @staticmethod
     def get_home_currency() -> Currency:
@@ -63,13 +67,13 @@ class GlobalPreferences(models.Model):
     @staticmethod
     def set_subject_classification_vocabulary(vocabulary: VocabularyProtocol) -> None:
         prefs, _ = GlobalPreferences.objects.get_or_create()
-        prefs.default_subject_classification_vocabulary_id = vocabulary.id
+        prefs.subject_classification_vocabulary_id = vocabulary.id
         prefs.save()
 
     @staticmethod
     def set_publication_type_vocabulary(vocabulary: VocabularyProtocol) -> None:
         prefs, _ = GlobalPreferences.objects.get_or_create()
-        prefs.default_publication_type_vocabulary_id = vocabulary.id
+        prefs.article_publication_type_vocabulary_id = vocabulary.id
         prefs.save()
 
     @staticmethod
