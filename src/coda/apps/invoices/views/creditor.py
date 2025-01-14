4160645db63ec76b88b02bae62ed5b8f4c00f4d1
Sven Marcus <s.marcus@outlook.de>
2025-01-14 17:13:45 +0000

    fix(invoices/views/creditor): require login for creditor views
    for some reason this causes a mypy error with the fields set to "__all__" despite being a valid value

--- src/coda/apps/invoices/views/creditor.py
+++ src/coda/apps/invoices/views/creditor.py
@@ -1,28 +1,29 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from coda.apps.invoices.forms import CreditorForm
 from coda.apps.invoices.models import Creditor
 
 
-class CreditorListView(ListView[Creditor]):
+class CreditorListView(LoginRequiredMixin, ListView[Creditor]):
     model = Creditor
     template_name = "invoices/creditors/list.html"
     context_object_name = "creditors"
 
 
-class CreditorDetailView(DetailView[Creditor]):
+class CreditorDetailView(LoginRequiredMixin, DetailView[Creditor]):
     model = Creditor
     template_name = "invoices/creditors/detail.html"
     context_object_name = "creditor"
 
 
-class CreditorCreateView(CreateView[Creditor, CreditorForm]):
+class CreditorCreateView(LoginRequiredMixin, CreateView[Creditor, CreditorForm]):
     model = Creditor
     template_name = "generic_form_view.html"
-    fields = "__all__"
+    fields = "__all__"  # type: ignore
 
 
-class CreditorUpdateView(UpdateView[Creditor, CreditorForm]):
+class CreditorUpdateView(LoginRequiredMixin, UpdateView[Creditor, CreditorForm]):
     model = Creditor
     template_name = "generic_form_view.html"
-    fields = "__all__"
+    fields = "__all__"  # type: ignore
