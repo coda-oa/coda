23b3e80dfba0c97ced20605fcfa41b57f82f9207
Sven Marcus <s.marcus@outlook.de>
2025-02-28 08:36:57 +0000

    fix(Publisher): get_absolute_url returns url to list view with query string

--- src/coda/apps/publishers/models.py
+++ src/coda/apps/publishers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.http import urlencode
 
 
 class Publisher(models.Model):
@@ -8,7 +9,10 @@ class Publisher(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self) -> str:
-        return reverse("publishers:detail", kwargs={"pk": self.pk})
+        """Publishers currently have no detail view. Therefore, the list view is returned."""
+        listview_url = reverse("publishers:list")
+        encoded_query_string = urlencode({"query": self.name})
+        return f"{listview_url}?{encoded_query_string}"
 
     def __str__(self) -> str:
         return self.name
