d6c67674f35c849153c9274e3c189f4b4fef9fed
Sven Marcus <s.marcus@outlook.de>
2025-03-17 08:05:34 +0000

    test(BlockCheck): add tests for block check

---
+++ tests/checks/test_blockcheck.py
@@ -0,0 +1,102 @@
+import pytest
+
+from coda.apps.blocklist.models import BlockList
+from coda.apps.fundingrequests import repository
+from coda.apps.journals.models import Journal
+from coda.apps.publishers.models import Publisher
+from coda.checks.blockcheck import BlockCheck
+from coda.checks.checklist import CheckFailed, CheckSuccessful
+from coda.contract import PublisherId
+from coda.fundingrequest import FundingRequest
+from coda.publication import JournalId, Monograph, Publication
+from tests import domainfactory, modelfactory
+
+
+@pytest.mark.django_db
+def test__fundingrequest_for_article_in_blocked_journal__fails_blockcheck() -> None:
+    journal = blocked_journal()
+    fundingrequest = fundingrequest_for_article(journal)
+
+    sut = BlockCheck()
+    result = sut(fundingrequest)
+
+    assert isinstance(result, CheckFailed)
+
+
+@pytest.mark.django_db
+def test__fundingrequest_for_article_for_blocked_publisher__fails_blockcheck() -> None:
+    journal = journal_of_blocked_publisher()
+    fundingrequest = fundingrequest_for_article(journal)
+
+    sut = BlockCheck()
+    result = sut(fundingrequest)
+
+    assert isinstance(result, CheckFailed)
+
+
+@pytest.mark.django_db
+def test__fundingrequest_for_article_in_unblocked_journal__passes_blockcheck() -> None:
+    journal = modelfactory.journal()
+    fundingrequest = fundingrequest_for_article(journal)
+
+    sut = BlockCheck()
+    result = sut(fundingrequest)
+
+    assert isinstance(result, CheckSuccessful)
+
+
+@pytest.mark.django_db
+def test__fundingrequest_for_monograph_of_blocked_publisher__fails_blockcheck() -> None:
+    publisher = blocked_publisher()
+    fundingrequest = fundingrequest_for_monograph(publisher)
+
+    sut = BlockCheck()
+    result = sut(fundingrequest)
+
+    assert isinstance(result, CheckFailed)
+
+
+@pytest.mark.django_db
+def test__fundingreuqest_for_monograph_of_unblocked_publisher__passes_blockcheck() -> None:
+    publisher = modelfactory.publisher()
+    fundingrequest = fundingrequest_for_monograph(publisher)
+
+    sut = BlockCheck()
+    result = sut(fundingrequest)
+
+    assert isinstance(result, CheckSuccessful)
+
+
+def fundingrequest_for_article(journal: Journal) -> FundingRequest[Publication]:
+    article = domainfactory.publication(journal=JournalId(journal.id))
+    fundingrequest = FundingRequest.new(article, domainfactory.payment())
+    fundingrequest.id = repository.save(fundingrequest)
+    return fundingrequest
+
+
+def blocked_journal() -> Journal:
+    journal = modelfactory.journal()
+    blocklist = BlockList.objects.get()
+    blocklist.block_journal(journal, reason="PREDATORY")
+    return journal
+
+
+def journal_of_blocked_publisher() -> Journal:
+    journal = modelfactory.journal()
+    blocklist = BlockList.objects.get()
+    blocklist.block_publisher(journal.publisher)
+    return journal
+
+
+def fundingrequest_for_monograph(publisher: Publisher) -> FundingRequest[Monograph]:
+    monograph = domainfactory.monograph(publisher=PublisherId(publisher.id))
+    fundingrequest = FundingRequest.new(monograph, domainfactory.payment())
+    fundingrequest.id = repository.save(fundingrequest)
+    return fundingrequest
+
+
+def blocked_publisher() -> Publisher:
+    publisher = modelfactory.publisher()
+    blocklist = BlockList.objects.get()
+    blocklist.block_publisher(publisher)
+    return publisher
