39421e70af207fc89261ec20e250fec92d082803
LindaAchilles <achilles.linda@gmail.com>
2025-07-14 08:57:53 +0000

    fix(infopage): added require_GET to infopage view

--- src/coda/apps/infopage/views.py
+++ src/coda/apps/infopage/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from django.views.decorators.http import require_GET
+
+@require_GET
 def index(request):
     return render(request, "infopage/info_page.html")
