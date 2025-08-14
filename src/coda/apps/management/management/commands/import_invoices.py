81098d31375f9e6e26cf02b0459d2dff23b7dbbf
Sven Marcus <s.marcus@outlook.de>
2025-08-14 16:36:01 +0000

    feat(import_invoices): adds command to import invoices

---
+++ src/coda/apps/management/management/commands/import_invoices.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from typing import Any
+
+from django.core.management import BaseCommand, CommandParser
+
+from coda.apps.invoices import importservice
+
+
+class Command(BaseCommand):
+    """
+    Command to import funding requests from a JSON file.
+    """
+
+    help = "Import funding requests from a JSON file."
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            "json_file",
+            type=str,
+            help="Path to the JSON file containing funding requests.",
+        )
+
+    def handle(self, *args: Any, **options: Any) -> None:
+        file = options["json_file"]
+        self.stdout.write(f"Importing invoices from {file}...")
+        with Path(file).open("r") as f:
+            result = importservice.import_invoices(f)
+            for number, errors in result.errors.items():
+                self.stdout.write(self.style.ERROR(f"Error importing invoice: {number}"))
+                self.stdout.write(self.style.ERROR(f"Errors: {', '.join(errors)}"))
+
+        self.stdout.write(self.style.SUCCESS("Invoices imported successfully."))
