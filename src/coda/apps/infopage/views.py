b787d708d6cc409e9ca58e884230623c5a942f47
Sven Marcus <s.marcus@outlook.de>
2025-09-02 12:14:42 +0000

    style(infopage): adds missing type annotations to info page

--- src/coda/apps/infopage/views.py
+++ src/coda/apps/infopage/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-
 from django.views.decorators.http import require_GET
 
+
 @require_GET
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return render(request, "infopage/info_page.html")
