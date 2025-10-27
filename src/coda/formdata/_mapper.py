ce6c094aef21a93501823abdcf303ab12f613f83
Sven Marcus <s.marcus@outlook.de>
2025-10-27 17:06:27 +0000

    feat(formdata): completed implementation of map_to_dict

--- src/coda/formdata/_mapper.py
+++ src/coda/formdata/_mapper.py
@@ -1,9 +1,9 @@
-from collections.abc import Sequence
 from typing import Any, TypeVar
 
 import pydantic
-from ._keys import KeyMatcher
 
+from ._keys import KeyMatcher
+from ._to_dict import flatten_dict
 
 MAX_SEQUENCE_LENGTH = 10000
 
@@ -12,10 +12,10 @@ M = TypeVar("M", bound=pydantic.BaseModel)
 
 def map_to_model(model: type[M], data: dict[str, Any], prefix: str = "") -> M:
     from ._processors import (
+        MappingFieldProcessor,
+        ModelFieldProcessor,
         ProcessorChain,
         SequenceFieldProcessor,
-        ModelFieldProcessor,
-        MappingFieldProcessor,
     )
 
     key_matcher = KeyMatcher()
@@ -34,32 +34,5 @@ def map_to_model(model: type[M], data: dict[str, Any], prefix: str = "") -> M:
     return model(**processed_data)
 
 
-def map_to_dict(obj: pydantic.BaseModel) -> dict[str, str]:
-    dump = obj.model_dump()
-    mapped: dict[str, str] = {}
-    for k, v in dump.items():
-        mapped.update(_map_field_to_dict(k, v))
-
-    return mapped
-
-
-def _map_field_to_dict(k: str, v: Any) -> dict[str, str]:
-    mapped = {}
-    match v:
-        case str():
-            mapped[k] = str(v)
-        case Sequence():
-            last_dash = k.rfind("-")
-            if last_dash != -1:
-                len_k = k[:last_dash] + "-#" + k[last_dash:]
-            else:
-                len_k = "#-" + k
-            mapped[len_k] = str(len(v))
-            for i, item in enumerate(v, start=1):
-                mapped.update(_map_field_to_dict(f"{k}-{i}", item))
-        case dict():
-            for sub_k, sub_v in v.items():
-                mapped.update(_map_field_to_dict(f"{k}-{sub_k}", sub_v))
-        case _:
-            mapped[k] = str(v)
-    return mapped
+def map_to_dict(obj: pydantic.BaseModel, skip_none: bool = False) -> dict[str, str]:
+    return flatten_dict(obj.model_dump(), prefix="", skip_none=skip_none)
