e52190cd6639985aa24c2e80c07f24db742f4a68
Sven Marcus <s.marcus@outlook.de>
2026-01-05 13:56:36 +0000

    refactor(test_invoice_repository): use tuple literal

--- tests/invoices/test_invoice_repository.py
+++ tests/invoices/test_invoice_repository.py
@@ -114,12 +114,10 @@ def full_invoice() -> Invoice:
 
     invoice = domainfactory.invoice(
         creditor=CreditorId(creditor_id),
-        positions=tuple(
-            [
-                *[publication_position(publication) for publication in publications],
-                *[contract_position(contract) for contract in contracts],
-                *[free_position() for _ in range(3)],
-            ]
+        positions=(
+            *[publication_position(publication) for publication in publications],
+            *[contract_position(contract) for contract in contracts],
+            *[free_position() for _ in range(3)],
         ),
     )
 
