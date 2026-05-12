14a685a00f7f16a31bd38bca068ae2132c6f7994
Sven Marcus <s.marcus@outlook.de>
2026-05-12 12:58:19 +0000

    refactor(creditors/funding-sources): refactor list items for more unified layout

--- src/coda/apps/invoices/views/creditor.py
+++ src/coda/apps/invoices/views/creditor.py
@@ -15,7 +15,7 @@ from coda.apps.breadcrumbs.decorators import breadcrumb
 @breadcrumb("Creditors", parent_url_name="invoices:finances_home")
 class CreditorListView(LoginRequiredMixin, SimpleSearchEntityListView[Creditor]):
     model = Creditor
-    paginate_by = 20
+    paginate_by = 10
     entity_name = "Creditors"
     entity_create_url = "invoices:creditor_create"
     entity_list_item_template = "invoices/creditors/list.html"
