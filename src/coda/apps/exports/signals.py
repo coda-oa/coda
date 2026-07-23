c58fdc90678539b1c584f5f5464cf0a6a40b7d57
LindaAchilles <achilles.linda@gmail.com>
2026-07-23 08:07:14 +0000

    feat(contract-csv-export/model): added model and delete signal for contract csv export
    #236

--- src/coda/apps/exports/signals.py
+++ src/coda/apps/exports/signals.py
@@ -3,12 +3,20 @@ from typing import Any
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from coda.apps.exports.models import FundingRequestCSVExport
+from coda.apps.exports.models import ContractCSVExport, FundingRequestCSVExport
 
 
 @receiver(post_delete, sender=FundingRequestCSVExport)
 def delete_csv_file_on_export_delete(
     sender: type[FundingRequestCSVExport], instance: FundingRequestCSVExport, **kwargs: Any
+) -> None:
+    if instance.csv_file:
+        instance.csv_file.delete(save=False)
+
+
+@receiver(post_delete, sender=ContractCSVExport)
+def delete_csv_file_on_contract_export_delete(
+    sender: type[ContractCSVExport], instance: ContractCSVExport, **kwargs: Any
 ) -> None:
     if instance.csv_file:
         instance.csv_file.delete(save=False)
