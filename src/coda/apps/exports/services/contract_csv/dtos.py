72cce81326b5adffd5cb14c2ef7b13c41397f8ee
LindaAchilles <achilles.linda@gmail.com>
2026-07-23 08:26:28 +0000

    feat(contract-csv-export/dtos): added dtos for later mapping contract information
    #236

---
+++ src/coda/apps/exports/services/contract_csv/dtos.py
@@ -0,0 +1,25 @@
+import datetime
+
+from pydantic import BaseModel
+from coda.contexts.finance.dto.import_dtos import InvoiceImportDto
+
+
+class ContractLinkDto(BaseModel):
+    type: str
+    value: str
+
+
+class ContractInfoDto(BaseModel):
+    name: str
+    start_date: datetime.date | None = None
+    end_date: datetime.date | None = None
+    publishers: list[str] = []
+    journals: list[str] = []
+    publication_billing: str = "Individually"
+    active: bool = True
+    links: list[ContractLinkDto] = []
+
+
+class ContractCSVExportDto(BaseModel):
+    contract: ContractInfoDto
+    invoices: list[InvoiceImportDto]
