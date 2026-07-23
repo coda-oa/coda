c03c87763968d605b795b759b4c22c272bfc20ef
LindaAchilles <achilles.linda@gmail.com>
2026-07-23 13:46:06 +0000

    feat(contract-csv-export/mappers): added contract mapper layer (models -> dtos)
    #236

---
+++ src/coda/apps/exports/services/contract_csv/mappers.py
@@ -0,0 +1,49 @@
+from coda.apps.contracts.models import Contract
+from coda.apps.exports.services.contract_csv.dtos import (
+    ContractCSVExportDto,
+    ContractInfoDto,
+    ContractLinkDto,
+)
+from coda.apps.exports.services.fundingrequest_csv.mappers import map_invoice_to_dto
+from coda.apps.invoices.models import Invoice
+from coda.contexts.finance.dto.import_dtos import InvoiceImportDto
+
+
+def map_contract_to_export_dto(contract: Contract) -> ContractCSVExportDto:
+    contract_dto = map_contract_to_dto(contract)
+
+    invoices = get_relevant_invoices_for_contract(contract)
+
+    invoice_dtos: list[InvoiceImportDto] = []
+    for invoice in invoices:
+        scoped_positions = [pos for pos in invoice.positions.all() if pos.contract == contract]
+        invoice_dtos.append(
+            map_invoice_to_dto(invoice, invoice_positions=scoped_positions, funding_request=None)
+        )
+
+    return ContractCSVExportDto(
+        contract=contract_dto,
+        invoices=invoice_dtos,
+    )
+
+
+def map_contract_to_dto(contract_model: Contract) -> ContractInfoDto:
+
+    return ContractInfoDto(
+        name=contract_model.name,
+        start_date=contract_model.start_date,
+        end_date=contract_model.end_date,
+        publishers=[publisher.name for publisher in contract_model.publishers.all()],
+        journals=[journal.title for journal in contract_model.journals.all()],
+        publication_billing=contract_model.publication_billing,
+        active=contract_model.active_status,
+        links=[
+            ContractLinkDto(type=link.type.name, value=link.value)
+            for link in contract_model.links.all()
+        ],
+    )
+
+
+def get_relevant_invoices_for_contract(contract: Contract) -> list[Invoice]:
+    invoices = {pos.invoice for pos in contract.position_set.all() if pos.invoice is not None}
+    return list(invoices)
