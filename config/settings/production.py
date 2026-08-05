3779ee4a185a9170ee40eac6eed754ad6435e70e
LindaAchilles <achilles.linda@gmail.com>
2026-08-05 12:41:27 +0000

    fix(production.env): added default storage configuration to production env file

--- config/settings/production.py
+++ config/settings/production.py
@@ -123,6 +123,9 @@ LOGGING = {
 
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
